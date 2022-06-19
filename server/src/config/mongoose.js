797eff919fc8717569b55dde985a73afa9948248
Tella Abdulrasheed <rashtell2@gmail.com>
2022-06-19 03:05:18 +0100

    chore: log db connection error

--- server/src/config/mongoose.js
+++ server/src/config/mongoose.js
@@ -1,28 +1,38 @@
-const mongoose = require("mongoose");
-export const AutoIncrement = require('mongoose-sequence')(mongoose);
+import { LogError } from "../app/Domain/Logger";
+import stopRequests from "../app/Middleware/stopRequests";
 
-module.exports = app => {
-    mongoose.connect('mongodb+srv://figfin_admin:finesse2.0@cluster0.bcvhx.mongodb.net/?retryWrites=true&w=majority', {
+const mongoose = require("mongoose");
+export const AutoIncrement = require("mongoose-sequence")(mongoose);
+
+module.exports = (app) => {
+  mongoose
+    .connect(
+      "mongodb+srv://figfin_admin:finesse2.0@cluster0.bcvhx.mongodb.net/?retryWrites=true&w=majority",
+      {
         useUnifiedTopology: true,
         useNewUrlParser: true,
-        useFindAndModify: false
-    })
-    .then(res => console.log("mongo conneceted"))
-    .catch(err => console.log(err))
+        useFindAndModify: false,
+      }
+    )
+    .then((res) => console.log("mongo conneceted"))
+    .catch((err) => {
+      console.log(err.message);
+      LogError(err.message);
+    });
 
-    mongoose.Promise = global.Promise;
-    
-    process.on("SIGINT", cleanup);
-    process.on("SIGTERM", cleanup);
-    process.on("SIGHUP", cleanup);
-    
-    if (app) {
-        app.set("mongoose", mongoose);
-    }
+  mongoose.Promise = global.Promise;
+
+  process.on("SIGINT", cleanup);
+  process.on("SIGTERM", cleanup);
+  process.on("SIGHUP", cleanup);
+
+  if (app) {
+    app.set("mongoose", mongoose);
+  }
 };
 
 function cleanup() {
-    mongoose.connection.close(function () {
-        process.exit(0);
-    });
+  mongoose.connection.close(function () {
+    process.exit(0);
+  });
 }
