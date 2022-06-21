ac7d50b962aaaaa2dbc8dc19186c527efb1fb692
Tella Abdulrasheed <rashtell2@gmail.com>
2022-06-21 14:15:53 +0100

    chore: added build file

---
+++ client/build/precache-manifest.4b49a82e7177d47ef4ddc2329403a7fb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a830ea292c8edda735bc90ba30e334fe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d5338c8959b9aa256330",
+    "url": "./static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "a74108a923a37e9bfb24",
+    "url": "./static/css/main.9fc79095.chunk.css"
+  },
+  {
+    "revision": "d5338c8959b9aa256330",
+    "url": "./static/js/2.4751e6b7.chunk.js"
+  },
+  {
+    "revision": "743dc2a6585b3183bc471fe652c0f971",
+    "url": "./static/js/2.4751e6b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a74108a923a37e9bfb24",
+    "url": "./static/js/main.e97c72a8.chunk.js"
+  },
+  {
+    "revision": "772efe5590ad79986fbf3a1db778be8b",
+    "url": "./static/js/main.e97c72a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b39408f4e659f593d43",
+    "url": "./static/js/runtime-main.1c1250c6.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "./static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "./static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "./static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "dedf26989fdd78c33cb9ae664a604d6c",
+    "url": "./static/media/Pe-icon-7-stroke.dedf2698.svg"
+  },
+  {
+    "revision": "6d362a8d1a8435996295c22328718bd6",
+    "url": "./static/media/logo.6d362a8d.png"
+  },
+  {
+    "revision": "6be21e8a1b7d63048728851c6003e189",
+    "url": "./static/media/sidebar-1.6be21e8a.jpg"
+  }
+]);
